dfc1beed4ec9349005f43c4dc29f40f48d3b6d02
Scott Smith <scottksmith95@gmail.com>
2013-02-20 14:46:23 -0800

    fixed css bugs

--- jquery.coderbits.js
+++ jquery.coderbits.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     (function () {
         var addCommas = function (nStr) {
             nStr += '';
-            x = nStr.split('.');
-            x1 = x[0];
-            x2 = x.length > 1 ? '.' + x[1] : '';
+            var x = nStr.split('.');
+            var x1 = x[0];
+            var x2 = x.length > 1 ? '.' + x[1] : '';
             var rgx = /(\d+)(\d{3})/;
             while (rgx.test(x1)) {
                 x1 = x1.replace(rgx, '$1' + ',' + '$2');
