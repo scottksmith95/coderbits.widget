2437fe8e79918e8a5c57727dbd47e0a8aaabd9fa
Donovan Glover <UltimateDonovan@gmail.com>
2013-02-21 01:18:20 -0500

    implemented addCommas() algorithm

--- coderbits.js
+++ coderbits.js
@@ -1,4 +1,14 @@
 ;(function(window, undefined) {
   "use strict";
-
+	var addCommas = function(nStr) {
+		nStr += '';
+		var x = nStr.split('.');
+		var x1 = x[0];
+		var x2 = x.length > 1 ? '.' + x[1] : '';
+		var rgx = /(\d+)(\d{3})/;
+		while (rgx.test(x1)) {
+			x1 = x1.replace(rgx, '$1' + ',' + '$2');
+		}
+		return x1 + x2;
+	}
 })(window);
