4a695eb4b08ab5eda377b4b032f807bcb812e77f
Donovan Glover <UltimateDonovan@gmail.com>
2013-02-21 02:04:48 -0500

    test case with only buildSummary()

--- coderbits.js
+++ coderbits.js
@@ -12,6 +12,26 @@
 		}
 		return x1 + x2;
 	};
+
+	var buildSummary = function(data) {
+		var content = '';
+			content += '<div id="coderbits-summary">';
+			content += '<div id="coderbits-avatar">';
+			content += '<a href="' + data.link + '" target="_parent">';content += '<img alt="' + data.name + '" class="avatar" src="https://secure.gravatar.com/avatar/' + data.gravatar_hash + '?d=https%3A%2F%2Fcoderbits.com%2Fimages%2Fgravatar.png&r=PG&s=32">';
+			content += '</a>';
+			content += '</div>';
+			content += '<div id="coderbits-overview">';
+			content += '<h1>';
+			content += '<a href="' + data.link + '" target="_parent" title="' + data.name + '">' + data.name + '</a>';
+			content += '</h1>';
+			content += '<p>';
+			content += '<span>' + data.title + '</span>';
+			content += '</p>';
+			content += '</div>';
+			content += '</div>';
+		return content;
+	}
+
 	var request = function(url) {
 		var script = document.getElementsByTagName("script")[0];
 		var handler = document.createElement("script");
@@ -22,9 +42,15 @@
 	var global = "coderbits";
 	window[global] = function(data) {
 		console.log(data);
+		var content = '';
+			content += buildSummary(data);
+			//content += buildSkills(data);
+			//content += buildStats(data);
+			//content += buildBadges(data);
+		document.getElementById(global).innerHTML = content;
 		delete window[global];
 	}
 
-	var username = document.getElementById("coderbits").getAttribute("data-coderbits-username");
+	var username = document.getElementById(global).getAttribute("data-coderbits-username");
 	request("https://coderbits.com/" + username + ".json?callback=coderbits");
 })(window, document);
